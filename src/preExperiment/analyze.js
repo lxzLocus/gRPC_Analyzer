262578ff787ad64bf5b64ecc4cf5df33b1a575e8
lxzLocus <unicorngundam03@gmail.com>
2024-07-11 18:14:15 +0000

    Return値追加

--- src/preExperiment/analyze.js
+++ src/preExperiment/analyze.js
@@ -7,8 +7,19 @@ const projectFolder = path.join(__dirname, '../../dataset/grpc_openai');
 // ファイルパターンを定義
 const filePatterns = ['_grpc.pb.go', '.pb.go', '.proto'];
 
+
+
+let foundFiles = findFiles(projectFolder);
+console.log(foundFiles);
+
+
+
+/*Functions*/
+
 // 再帰的にディレクトリを探索する関数
 function findFiles(dir) {
+    let res = [];
+
     // ディレクトリの内容を読み取る
     const files = fs.readdirSync(dir);
 
@@ -22,12 +33,12 @@ function findFiles(dir) {
 
             if (stat.isDirectory()) {
                 // ディレクトリの場合、再帰的に探索
-                findFiles(fullPath);
+                res = res.concat(findFiles(fullPath));
             } else if (stat.isFile()) {
                 // ファイルが指定されたパターンに一致するかチェック
                 for (const pattern of filePatterns) {
                     if (fullPath.endsWith(pattern)) {
-                        console.log(`Found ${pattern} file: ${fullPath}`);
+                        res.push({ pattern, fullPath });
                         break; // 一つのパターンに一致したら他のパターンはチェックしない
                     }
                 }
@@ -36,7 +47,5 @@ function findFiles(dir) {
             console.error(`Error accessing ${fullPath}: ${err.message}`);
         }
     }
+    return res;
 }
-
-// プロジェクトフォルダを探索
-findFiles(projectFolder);
