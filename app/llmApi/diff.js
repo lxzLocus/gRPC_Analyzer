5f1e0fa3d8f39790121c2c22652d01687c3c2992
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-09-15 01:17:31 +0000

    [Add] Diffプログラム

--- app/llmApi/diff.js
+++ app/llmApi/diff.js
@@ -2,9 +2,21 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-// 2つのディレクトリ
-const dir1 = '/app/dataset/clone/go-micro-services/pullrequest/Add_go_output_directory_for_protos/merge_38'
-const dir2 = '/app/dataset/clone/go-micro-services/pullrequest/Add_go_output_directory_for_protos/premerge_38';
+// 1つの入力ディレクトリ
+const inputDir = '/app/dataset/clone/alibabacloud-microservice-demo/pullrequest/feat-_multiple_framework_go_http_demo';
+
+// `premerge`と`merge`ディレクトリを検出
+const premergeDir = fs.readdirSync(inputDir).find(dir => dir.startsWith('premerge'));
+const mergeDir = fs.readdirSync(inputDir).find(dir => dir.startsWith('merge'));
+
+if (!premergeDir || !mergeDir) {
+    console.error('premerge または merge ディレクトリが見つかりません');
+    process.exit(1);
+}
+
+// フルパスの生成
+const premergePath = path.join(inputDir, premergeDir);
+const mergePath = path.join(inputDir, mergeDir);
 
 // ファイルを再帰的に探索する関数（.gitディレクトリを除外）
 function getFilesRecursive(dir) {
@@ -29,17 +41,17 @@ function getFilesRecursive(dir) {
 }
 
 // 2つのディレクトリから全ファイルリストを取得（.gitディレクトリを除外）
-const files1 = getFilesRecursive(dir1);
-const files2 = getFilesRecursive(dir2);
+const premergeFiles = getFilesRecursive(premergePath);
+const mergeFiles = getFilesRecursive(mergePath);
 
 // 同名ファイルがある場合にdiffを取る
-files1.forEach(file1 => {
-    const relativePath = path.relative(dir1, file1);
-    const file2 = path.join(dir2, relativePath);
+premergeFiles.forEach(file1 => {
+    const relativePath = path.relative(premergePath, file1);
+    const file2 = path.join(mergePath, relativePath);
 
     if (fs.existsSync(file2)) {
-        // 同名ファイルが見つかったら、diffコマンドを実行
-        exec(`diff ${file1} ${file2}`, (err, stdout, stderr) => {
+        // 同名ファイルが見つかったら、diffコマンドを実行（パスをクォーテーションで囲む）
+        exec(`diff "${file1}" "${file2}"`, (err, stdout, stderr) => {
             if (err) {
                 // 差分が見つかると終了コード1になる
                 if (err.code === 1) {
